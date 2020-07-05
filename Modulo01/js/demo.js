2743129ed4684250021f15e94d7323516acf0826
manudous <67704877+manudous@users.noreply.github.com>
2020-07-05 17:02:10 +0200

    Add files via upload

---
+++ Modulo01/js/demo.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var nombre = document.getElementById('name');
+var apellido = document.getElementById('lastName');
+var imagen = document.getElementById('avatar');
+
+nombre.value = 'Manuel';
+apellido.value = 'Dorado';
+imagen.src = 'img/undraw_profile_pic_ic5t.png';
